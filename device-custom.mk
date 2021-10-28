b5b8922204a35785d66ffb76c4b6b649eb2cf239
Chirayu Desai <chirayudesai1@gmail.com>
2021-10-28 17:37:57 +0530

    Set up EUICC permissions
    Co-authored-by: Ethan Chen <intervigil@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Change-Id: I093cd52996b11b88343c474e85d34595b10f5c62
    Signed-off-by: spezi77 <spezi7713@gmail.com>

--- device-custom.mk
+++ device-custom.mk
@@ -1,7 +1,12 @@
 #
-# Copyright (C) 2023 Another KitchenSink Project
+# Copyright (C) 2023-2025 Another KitchenSink Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 DEVICE_PACKAGE_OVERLAYS += device/google/pantah/overlay-custom
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+    $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
