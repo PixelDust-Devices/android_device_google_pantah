1f174e1a3c0c588948b884c214a1b761699b0966
Chirayu Desai <chirayudesai1@gmail.com>
2022-03-12 04:11:07 +0530

    Build EuiccSupportPixelOverlay
    Change-Id: I46db4122b89798457338f6751982865571529ba6
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- device-custom.mk
+++ device-custom.mk
@@ -8,3 +8,6 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/pantah/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
