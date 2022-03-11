996c9aab537482c8e73564ef8ea17575afd4562a
Chirayu Desai <chirayudesai1@gmail.com>
2022-03-12 04:11:07 +0530

    Build EuiccSupportPixelOverlay
    Change-Id: I46db4122b89798457338f6751982865571529ba6
    Signed-off-by: spezi77 <spezi7713@gmail.com>

--- device-custom.mk
+++ device-custom.mk
@@ -11,6 +11,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
 
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
 # HBM
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
