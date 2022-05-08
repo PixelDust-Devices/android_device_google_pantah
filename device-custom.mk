53797d57f19f73b9eba1cd817689ff6da0c59e90
Michael Bestas <mkbestas@gmail.com>
2022-05-08 03:45:18 +0300

    Add privapp permissions for HbmSVManager
    Change-Id: Ieab465f478424d8a936519250b40832f1c6bbc33
    Signed-off-by: spezi77 <spezi7713@gmail.com>

--- device-custom.mk
+++ device-custom.mk
@@ -10,3 +10,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/pantah/overlay-custom
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
+# HBM
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
