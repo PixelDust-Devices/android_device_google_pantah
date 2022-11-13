remusW55/device_google_redbull

Add privapp permissions for HbmSVManager
Change-Id: Ieab465f478424d8a936519250b40832f1c6bbc33

device-calyx.mk
@@ -11,6 +11,10 @@ PRODUCT_COPY_FILES += \
     device/google/redbull/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
     device/google/redbull/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml
 
+# HBM
+PRODUCT_COPY_FILES += \
+    device/google/redbull/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
+
 TARGET_PREBUILT_KERNEL := device/google/redbull-kernel/Image.lz4
 
 # LiveDisplay

PixelDust-Devices/android_device_google_pantah

device-calyx.mk
@@ -10,6 +10,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/google/pantah/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
 
+# HBM
+PRODUCT_COPY_FILES += \
+    device/google/pantah/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
+
 TARGET_PREBUILT_KERNEL := device/google/pantah-kernel/Image.lz4
 
 # Build necessary packages for vendor