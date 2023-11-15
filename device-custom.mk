2c339f7cc05a8c2ecd7d8021ee3375283ca1fef4
spezi77 <spezi7713@gmx.net>
2023-11-15 22:31:28 +0100

    Revert "Include wireless_charger makefile"
    This reverts commit 4fce9686499eeec1b9adcdb7f7140a058e196f2d.
    
    Breaks the build:
    FAILED: out/soong/.intermediates/system/sepolicy/recovery_sepolicy.cil/android_common/recovery_sepolicy.cil
    out/host/linux-x86/bin/checkpolicy -C -M -c 30 -o out/soong/.intermediates/system/sepolicy/recovery_sepolicy.cil/android_common/recovery_sepolicy.cil out/soong/.intermediate
    s/system/sepolicy/recovery_sepolicy.conf/android_common/recovery_sepolicy.conf # hash of input list: a316674d1dc0868a105f79e687a4b6449e6a8801cfa3253b9c426787c1c40d09
    device/google/gs-common/wireless_charger/sepolicy/hal_googlebattery.te:1:ERROR 'unknown type hal_googlebattery' at token ';' on line 90264:
    allow hal_googlebattery sysfs_wlc:dir { open getattr read search ioctl lock watch watch_reads };
    checkpolicy:  error(s) encountered while parsing configuration
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- device-custom.mk
+++ device-custom.mk
@@ -14,9 +14,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
-# wireless_charger HAL service
-include device/google/gs-common/wireless_charger/wireless_charger.mk
-
 # HBM
 PRODUCT_COPY_FILES += \
     device/google/pantah/permissions/permissions_com.android.hbmsvmanager.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.android.hbmsvmanager.xml
